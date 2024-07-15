72e976be23a4feb57c20547cf8589327a8b057d8
ProkopenkoAV <arty1987@mail.ru>
2024-07-15 21:28:09 +0300

    complited

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+
+        long underage = persons.stream().filter(person -> person.getAge() < 18).count();
+        System.out.println("Количество несовершеннолетних " + underage);
+
+        List<String> soldiers = persons.stream()
+                .filter(person -> person.getAge() >= 18 && person.getAge() <= 27)
+                .map(person -> person.getFamily())
+                .collect(Collectors.toList());
+
+        System.out.println(soldiers.get(4));
+
+        List<Person> workers = persons.stream()
+                .filter(person -> person.getAge() >= 18)
+                .filter(person -> (person.getSex() == Sex.MAN && person.getAge() <= 65) || (person.getSex() == Sex.WOMAN && person.getAge() <= 60))
+                .filter(person -> person.getEducation() == Education.HIGHER)
+                .sorted(Comparator.comparing(Person::getFamily))
+                .collect(Collectors.toList());
+
+        System.out.println(workers.get(6));
     }
 }
